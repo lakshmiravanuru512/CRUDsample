bb746a9a3ec869ff9f215d6bace135b1b66188ce
lakshmiravanuru512 <lakshmirevanuru512@gmail.com>
2023-09-28 09:17:19 +0530

    post example

--- crud_example/src/App.js
+++ crud_example/src/App.js
@@ -5,11 +5,25 @@ const baseURL = "https://jsonplaceholder.typicode.com/posts";
 
 export default function App() {
  const[post, setPost]=useState([]);
+
  useEffect(()=>{
   axios.get(baseURL).then((response)=>{
     setPost(response.data)
   })
  },[]);
+
+ function createPost(){
+  axios.post(baseURL,{
+    title:"Hello World",
+    body:"This is a new post call"
+  }).then((response)=>{
+    console.log(response.data);
+    //const newdata=[...post,response.data];
+   // setPost(newdata);
+   //setPost([...post,response.data]);
+   setPost((previousPosts=>[...previousPosts,response.data]))
+  })
+ }
 
   return (
     <div>
@@ -18,9 +32,11 @@ export default function App() {
           <li>
             <h1>{x.title}</h1>
             <p>{x.body}</p>
+            
           </li>
         )}
       </ul>
+      <button onClick={createPost}>create post</button>
       
     </div>
   );
